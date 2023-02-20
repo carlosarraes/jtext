e9aaf1ad1d5a306e65ea88a774c7f91521e755b4
carraes <carraeshb@gmail.com>
2023-02-20 14:12:56 -0300

    chore: initial files

---
+++ app/src/main/java/jtext/app/App.java
@@ -0,0 +1,7 @@
+package jtext.app;
+
+public class App {
+  public static void main(String[] args) {
+    LearnStrings.main(args);
+  }
+}
---
+++ app/src/main/java/jtext/app/LearnStrings.java
@@ -0,0 +1,8 @@
+package jtext.app;
+
+public class LearnStrings {
+  public static void main(String[] args) {
+    String s = "Hello Mundo!";
+    System.out.println(s);
+  }
+}
